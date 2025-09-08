1373d2bfcc1103885ccdffb0264c8b3aa99dddfc
heatherkrueger1234-design <heatherkrueger1234@gmail.com>
2025-09-08 20:58:57 +0600

    models.py

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,1 +1,17 @@
+# backend/app/models.py
+from pydantic import BaseModel
+from typing import Optional
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class UserIn(BaseModel):
+    username: str
+    password: str
+
+class UserOut(BaseModel):
+    username: str
+    email: Optional[str] = None
+    full_name: Optional[str] = None
 
